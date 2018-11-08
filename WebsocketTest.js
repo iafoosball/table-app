a81877b6bba40a7adea44aea2f8dfe1fa73ef829
jonas27 <jonas.burster@gmail.com>
2018-11-08 13:28:01 +0100

    WS stuff works

--- WebsocketTest.js
+++ WebsocketTest.js
@@ -1,61 +1,49 @@
 #!/usr/bin/env node
+var util = require('util');
+var EventEmitter = require('events');
+function MyClass() {
+    EventEmitter.call(this)
+}
+util.inherits(MyClass, EventEmitter);
 var WebSocketClient = require('websocket').client;
- 
+
 var client = new WebSocketClient();
 var match_id = 0;
 var speed = 0;
 
 var redGoals = 0;
 var blueGoals = 0;
- 
-client.on('connectFailed', function(error) {
+
+client.on('connectFailed', function (error) {
     console.log('Connect Error: ' + error.toString());
 });
- 
-client.on('connect', function(connection) {
-    console.log('Connected to IAfoosball');
-    connection.on('error', function(error) {
+
+client.on('connect', function (connection) {
+    console.log('WebSocket Client Connected');
+    connection.on('error', function (error) {
         console.log("Connection Error: " + error.toString());
     });
-    connection.on('close', function() {
+    connection.on('close', function () {
         console.log('echo-protocol Connection Closed');
     });
-    connection.on('message', function(message) {
+    connection.on('message', function (message) {
         if (message.type === 'utf8') {
-            message.on('data', function (chunk)
-		{
-			var postdata = JSON.parse(chunk);
-			if(postdata.Matchid){
-				match_id = postdata.Matchid;
-			}
-			console.log("matchid: "+match_id);
-			if(redGoals != postdata.score_red&&playing&&match_id!=0){
-				sleep.msleep(1200);
-				redGoals = postdata.score_red*1;
-				}
-			if(blueGoals != postdata.score_blue&&playing&&match_id!=0){
-				sleep.msleep(1200);
-				blueGoals = postdata.score_blue*1;
-				}
-			
-			console.log(chunk);
-		});
-	
+            handleServerData(message.utf8Data)
+        }
+    });
 
-	    requestPost.on('error', function(e){
-            console.log("Received: '" + message.utf8Data + "'");
-        });
-    };
-});
-    function sendNumber() {
-        console.log('Send test String')
-        connection.send ('{"command": "addGoal", "values"{ "side":"blue", "speed":' + 10 + ',"position": "attack"}}');
-     
-        
+    function handleServerData() {
+
     }
-sendNumber();
-     
+
+    function sendCommand(command) {
+        connection.send(command);
+    }
+
+    sendCommand('{"command": "addGoal", "values":{ "side":"blue", "speed":10,"position": "attack"}}');
+
 });
- 
-client.connect('ws://iafoosball.aau.dk:9003/tables/table-12');
-    console.log('Connecting to IAfoosball...');
+
+
+client.connect('ws://0.0.0.0:9003/tables/table-12');
+console.log('Connecting to IAfoosball...');
